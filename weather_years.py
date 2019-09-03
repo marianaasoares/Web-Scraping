4927436aac32dbb85baf70d570d0de3886537be9
Mariana Soares <mariana.dsantos@al.infnet.edu.br>
2019-09-03 08:35:19 -0300

    Weather years
    Calcula média anual das temperaturas entre 1980 até 1999

---
+++ weather_years.py
@@ -0,0 +1,14 @@
+import requests
+
+url="http://climatedataapi.worldbank.org/climateweb/rest/v1/country/mavg/tas/1980/1999/BRA.csv"
+
+# Usando requests
+csv = requests.get(url).text
+linhas = csv.splitlines()
+header = linhas[0]
+
+for l in range(1, len(linhas)):
+    colunas = linhas[l].split(',')
+    colunas = map(float, colunas[4:])
+    print(sum(list(colunas)))
+    break
